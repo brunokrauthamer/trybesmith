ca5464ed72d3ea096c2c5a45c5ef0848c6ab0225
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-11-18 01:51:07 -0300

    req06

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import productMiddleware from '../middlewares/product.middleware';
 
 const router = Router();
 
 const productController = new ProductController();
 
-router.post('/', productController.create.bind(productController));
+router.post(
+  '/',
+  productMiddleware.verifyName,
+  productMiddleware.verifyAmount,
+  productController.create.bind(productController),
+);
 
 router.get('/', productController.getAll.bind(productController));
 
